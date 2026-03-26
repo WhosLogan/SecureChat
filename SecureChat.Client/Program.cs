f116b3c6aa594d66a64dc837a2cb083d87f80bd8
WhosLogan <128313631+WhosLogan@users.noreply.github.com>
2026-03-26 10:48:41 -0400

    Initial Commit

---
+++ SecureChat.Client/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace SecureChat.Client;
+
+internal static class Program
+{
+    internal static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
---
+++ SecureChat.Server/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace SecureChat.Server;
+
+internal static class Program
+{
+    internal static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
