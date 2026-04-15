ffea77a4b03a61e9b820aadaad1ba72785316f9d
WhosLogan <128313631+WhosLogan@users.noreply.github.com>
2026-04-15 14:11:48 -0400

    Outline the client process

--- SecureChat.Client/Program.cs
+++ SecureChat.Client/Program.cs
@@ -7,15 +7,26 @@ internal static class Program
 {
     internal static async Task Main(string[] args)
     {
+        // Make sure we have our public and private key generated
+        
+        // Ask for username and password
+        
+        // Initialize connection to server
         var client = new WatsonTcpClient("127.0.0.1", 9000);
         client.Events.MessageReceived += OnMessageReceived;
+        client.Events.ServerConnected += OnServerConnected;
         client.Connect();
-        var success = await client.SendAsync(Encoding.Unicode.GetBytes("Hello World!"));
-        client.Disconnect();
+        
+        // Send credentials to become authenticated
+    }
+
+    private static void OnServerConnected(object? sender, ConnectionEventArgs e)
+    {
+        throw new NotImplementedException();
     }
 
     private static void OnMessageReceived(object? sender, MessageReceivedEventArgs e)
     {
-        Console.WriteLine("Received message on client");
+        throw new NotImplementedException();
     }
 }
