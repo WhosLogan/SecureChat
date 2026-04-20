f2ce793b9f3dae3423d81495132db4f6b25cc04d
WhosLogan <128313631+WhosLogan@users.noreply.github.com>
2026-04-20 13:07:43 -0400

    Add RSA encryption and decryption

--- SecureChat.Shared/Cryptography/Rsa/PrivateKey.cs
+++ SecureChat.Shared/Cryptography/Rsa/PrivateKey.cs
@@ -1,6 +1,33 @@
+using System.Security.Cryptography;
+
 namespace SecureChat.Shared.Cryptography.Rsa;
 
 public sealed class PrivateKey
 {
-    public PublicKey? PublicKey { get; } = null;
+    public PublicKey PublicKey { get; }
+    private readonly RSA _rsa;
+
+    public PrivateKey(string path)
+    {
+        _rsa = RSA.Create(4096);
+        try
+        {
+            _rsa.ImportFromPem(File.ReadAllText(path));
+        }
+        catch (FileNotFoundException)
+        {
+            // File does not exist, lets create a key then
+            SaveKey(path);
+        }
+        
+        PublicKey = new PublicKey(_rsa.ExportRSAPublicKeyPem());
+    }
+
+    private void SaveKey(string path)
+    {
+        File.WriteAllText(path, _rsa.ExportRSAPrivateKeyPem());
+    }
+
+    public byte[] Decrypt(byte[] data)
+        => _rsa.Decrypt(data, RSAEncryptionPadding.OaepSHA256);
 }
--- SecureChat.Shared/Cryptography/Rsa/PublicKey.cs
+++ SecureChat.Shared/Cryptography/Rsa/PublicKey.cs
@@ -1,6 +1,19 @@
+using System.Security.Cryptography;
+
 namespace SecureChat.Shared.Cryptography.Rsa;
 
 public sealed class PublicKey
 {
-    
+    private readonly RSA _rsa;
+    public string PublicKeyString { get; init; }
+
+    public PublicKey(string publicKey)
+    {
+        _rsa = RSA.Create();
+        _rsa.ImportFromPem(publicKey);
+        PublicKeyString = publicKey;
+    }
+
+    public byte[] Encrypt(byte[] data)
+        => _rsa.Encrypt(data, RSAEncryptionPadding.OaepSHA256);
 }
